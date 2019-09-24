6447bc9bd00f6669d39300c5c1dcb525b304f5b7
develHector <devel.hector@hotmail.com>
2019-09-24 07:18:13 -0500

    Nueva funcionalidad

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,77 @@
 #include <iostream>
 #include <thread>
+#include <atomic>
+#include <thread>
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #include "QuickStart.h"
 #include "Dog.h"
 #include "MulticastSender.h"
 #include "MulticastReceiver.h"
 #include "Dog.h"
 
+#include "ConcurrentCollections.hpp"
+#include "ParallelLib.hpp"
+#include "LoggingLib.hpp"
+
+const int NUM_THREADS = 8;
+Collections::ConcurrentDictionary<std::string, int> *dict3;
+
+void ThreadedInsert(int tid)
+{
+    // std::cout << "Launched by lambda " << tid << " thread " << std::this_thread::get_id() << std::endl;
+    Logging::Info("Launched by lambda " << tid << " thread " << std::this_thread::get_id());
+
+    for (int i = 0; i < 1000 * 1000; i++)
+    {
+        dict3->TryAdd(std::to_string(i), i);
+    }
+}
+
+void TestConcurrentCollections()
+{
+    std::cout << "TestConcurrentCollections..." << std::endl;
+
+    dict3 = new Collections::ConcurrentDictionary<std::string, int>();
+    dict3->PrintDictionary();
+
+    auto now1 = boost::posix_time::microsec_clock::local_time();
+
+    std::thread ThreadsDict[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        ThreadsDict[i] = std::thread(ThreadedInsert, i);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        ThreadsDict[i].join();
+    }
+
+    auto now2 = boost::posix_time::microsec_clock::local_time();
+    auto diff = (now2 - now1).total_milliseconds();
+
+    std::cout << "Size " << dict3->size() << " " << now1 << " - " << diff << " ms" << std::endl;
+
+    delete dict3;
+}
+
+void TestParallelLib()
+{
+    std::cout << "TestParallelLib..." << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
+
+    TestConcurrentCollections();
+    TestParallelLib();
+
+    std::cout << "Press any key to continue..." << std::endl;
+    std::getchar();
+
     std::cout << "Initializng the beast..." << std::endl;
 
     QuickStart::Run();
@@ -18,15 +82,14 @@ int main(int argc, char *argv[])
     auto treceiver2 = std::thread(MulticastReceiver::Run);
     std::this_thread::sleep_for(1s);
 
-
     auto tsender = std::thread(MulticastSender::Run);
     std::this_thread::sleep_for(1s);
-        
-    tsender.join();    
 
-    treceiver0.join();    
-    treceiver1.join();    
-    treceiver2.join();    
+    tsender.join();
+
+    treceiver0.join();
+    treceiver1.join();
+    treceiver2.join();
 
     char c;
     do
@@ -38,5 +101,4 @@ int main(int argc, char *argv[])
     return 0;
 
     std::cout << "Initializng the beast..." << std::endl;
-
 }
